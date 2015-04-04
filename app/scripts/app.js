b837834e0875cf0fcf9c9d39f728cd5a3a0b2d03
Akash A <akash1810@gmail.com>
2015-04-04 09:46:14 +0100

    Simplfying the design.
    - Removing meenu item.
    - Squared badges.
    - Using roboto font.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,7 @@
   'use strict';
 
   var app = document.querySelector('#app');
-  app.appName = 'Where its @akash1810';
-  app.page = 'home';
+  app.appName = 'where its @akash1810';
   app.profileUrl = '/profile.json';
 
 // wrap document so it plays nice with other libraries
