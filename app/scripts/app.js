d24205abc15b4b8e604e03712476919bc088cd55
Akash A <akash1810@gmail.com>
2015-04-05 11:05:53 +0100

    Rendering differently at 500px and below using core-media-query.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,10 @@
   'use strict';
 
   var app = document.querySelector('#app');
-  app.appName = 'where its @akash1810';
+  app.atIdentity = 'akash1810';
   app.profileUrl = '/profile.json';
+
+  document.title += app.atIdentity;
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
